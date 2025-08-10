a96f72b6aa802ccdc1654d40146efc7f0fb11c47
Bahareh Gholami <bahareh.gh001@gmail.com>
2025-08-10 12:30:10 +0330

    first commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import SideNav from "../ui/dashboard/sidenav"
+
+function Layout({children} : {children: React.ReactNode}){
+
+    return(
+        <div className="flex flex-col h-screen md:flex-row md-overflow-hidden">
+            <div className="w-full md:w-64"><SideNav /></div>
+            <div className="p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
+    )
+}
+
+export default Layout;
