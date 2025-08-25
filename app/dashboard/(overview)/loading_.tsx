5bead4decde70dbcc0357f8a00f244c42d280240
Bahareh Gholami <bahareh.gh001@gmail.com>
2025-08-25 11:06:00 +0330

    before install canary

---
+++ app/dashboard/(overview)/loading_.tsx
@@ -0,0 +1,13 @@
+import DashboardSkeleton from "@/app/ui/skeletons"
+
+export default function Loading(){
+    return(
+        <DashboardSkeleton />
+    )
+}
+
+
+
+
+
+// نام فایل باید تعییر کنه _ حذف بشه تا اجرا بشه
